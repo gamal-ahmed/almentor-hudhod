3fb0c19bcb04f0a8d6fbc6169630b11eb7820f8a
Ahmed Sayed <ahmed.sayed@almentor.net>
2025-04-01 16:51:24 +0200

    Fix: Update onTranscriptionsCreated handler to include sessionId parameter

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -92,7 +92,7 @@ const Index = () => {
               <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
                 <div className="lg:col-span-5 space-y-6">
                   <UploadConfigStep
-                    onTranscriptionsCreated={handleTranscriptionsCreated}
+                    onTranscriptionsCreated={(jobIdsArray, sessionId) => handleTranscriptionsCreated(jobIdsArray, sessionId)}
                     onStepComplete={() => {
                       // This is now handled immediately on file upload
                     }}
